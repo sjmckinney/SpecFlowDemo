e83195637b4458477aeafa47a40764dea58a8d48
sjmckinney <stevejmckinney@gmail.com>
2017-09-10 11:49:41 +0100

    Added Coypu driven browser test with required Nuget packages

---
+++ SpecFlowDemo/CoypuUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Coypu;
+using Coypu.Drivers;
+using System.Threading;
+
+namespace SpecFlowDemo
+{
+    [TestClass]
+    public class CoypuUnitTest
+    {
+        private BrowserSession browser;
+        private SessionConfiguration sessionConfiguration = new SessionConfiguration
+        {
+            AppHost = "www.34protons.co.uk",
+            SSL = true
+        };
+
+        public void BeforeTest()
+        {
+            sessionConfiguration.Browser = Browser.Chrome;
+            browser = new BrowserSession(sessionConfiguration);
+            Console.WriteLine($"browser configuration: {browser}");
+            var selenium = ((OpenQA.Selenium.Remote.RemoteWebDriver)browser.Native);
+            Console.WriteLine($"Underlying IWebDriver object configuration: {selenium}");
+        }
+
+        public void AfterTest()
+        {
+            browser.Dispose();
+        }
+
+        [TestMethod]
+        public void CoypuTest1()
+        {
+            BeforeTest();
+            browser.Visit("/");
+            Console.WriteLine($"Window title is {browser.Title}");
+            Console.WriteLine($"SessionConfiguration.SSL is {sessionConfiguration.SSL}");
+            Console.WriteLine($"Window url is {browser.Location}");
+            AfterTest();
+        }
+    }
+}
